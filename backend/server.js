a10761a8d1d0809cbddcc25b43d9da11423bf30f
sudhir-kumar999 <102527512+sudhir-kumar999@users.noreply.github.com>
2026-01-04 13:39:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3001",
+    origin: "https://smart-city-front-8dzh.onrender.com",
     credentials: true,
   })
 );
